f47a5b79363cbb02d44e5890254a2adf4a53a086
Howflet <howflet345@gmail.com>
2026-07-05 13:50:21 -0400

    feat: initialize project structure with Dub application and Finder extension targets

---
+++ Dub/Dub/DubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubApp.swift
+//  Dub
+//
+//  Created by Kevon Fletcher on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
